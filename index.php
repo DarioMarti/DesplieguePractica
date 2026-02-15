bdcd1c27570eb36d6635629f0f295ae095ac1abf
Dario <lpz.dariomarti@gmail.com>
2026-02-15 17:28:20 +0100

    cambios en el logotipo del index

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand" href="#">Filmflix</a>
+            <a class="navbar-brand" href="#">Filmbooks</a>
         </div>
         <ul class="nav navbar-nav">
             <li class="active"><a href="#">Home</a></li>
